e9b9d869baef44c9bf2b49f5c21720325d20386f
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2024-07-14 16:37:30 +0200

    improve example

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,45 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 
-const myRef = ref({
-  primitiveProperty: "initial",
-  nonPrimitiveProperty: new String("initial"),
-});
+const objectRef = ref({ foo: "bar" });
+const stringRef = ref("hello");
 
-function touchMyRef() {
-  myRef.value.primitiveProperty = "another"; // Setting primitiveProperty to another value
-  myRef.value.primitiveProperty = "initial"; // Setting primitiveProperty to the initial value before next tick happens
-  myRef.value.nonPrimitiveProperty = new String("another"); //  Setting nonPrimitiveProperty to another value
-  myRef.value.nonPrimitiveProperty = new String("initial"); //  Setting nonPrimitiveProperty to the initial value before next tick happens
+function touchRefs() {
+  objectRef.value.foo = "baz"; // Apply changes
+  objectRef.value.foo = "bar"; // Revert before next tick happens
+  stringRef.value = "world"; // Apply changes
+  stringRef.value = "hello"; // Revert before next tick happens
 }
 
 function callback(newValue: any, oldValue: any) {
   console.log(newValue, oldValue);
 }
 
-watch(() => myRef.value.primitiveProperty, callback); // Expected 😊: callback not triggered when calling `touchMyRef()`
-watch(() => myRef.value.nonPrimitiveProperty, callback); // NOT EXPECTED 🤨: callback TRIGGERED when calling `touchMyRef()`
-watch(() => myRef.value, callback); // Because of the unexpected second callback being triggered, I am not sure what to expect here, but the callback is not triggered when calling `touchMyRef()`,
+// The first watcher's getter function returns a primitive, the others return a non-primitive, which somehow decides if the watcher gets triggered or not.
+// Even though `touchRefs()` alters the refs, the changes are reverted before the next tick happens, so I wouldn't expect any of the watchers getting triggered.
+// Observing and comparing the behaviour of the third and fourth watcher, makes this even more confusing.
 
-setInterval(touchMyRef, 1000);
+watch(() => {
+  objectRef.value.foo; // using `stringRef.value` here, yields the same result
+  return Number(1);
+}, callback); // not triggered by `touchRefs()`, as expected 😊
+
+watch(() => {
+  objectRef.value.foo; // using `stringRef.value` here, yields the same result
+  return new Number(2);
+}, callback); // triggered by `touchRefs()`, not expected 🤔
+
+watch(() => {
+  objectRef.value;
+  return new Number(3);
+}, callback); // not triggered by `touchRefs()`, which is confusing based on the result of the second watcher 😦
+
+watch(() => {
+  stringRef.value;
+  return new Number(4);
+}, callback); // triggered by `touchRefs()`, which is even more confusing 🤯
+
+setInterval(touchRefs, 1000);
 </script>
+
+<template></template>
